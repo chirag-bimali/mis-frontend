9a99882d9c1062339acfb366efe03ff2f199cf30
chirag-bimali <70309029+chirag-bimali@users.noreply.github.com>
2026-08-16 14:47:51 +0545

    Refactor decision-making route to use shared component
    - Replace inline route component with shared Decision page component
    - Clean up unused code and formatting

--- src/routes/_app/data-collection/drafts/$caseId/household-profile/$householdId/decision-making.tsx
+++ src/routes/_app/data-collection/drafts/$caseId/household-profile/$householdId/decision-making.tsx
@@ -1,16 +1,12 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import Decision from "@pages/data-collection/drafts/household-profile/decision";
 
 export const Route = createFileRoute(
-  '/_app/data-collection/drafts/$caseId/household-profile/$householdId/decision-making',
+  "/_app/data-collection/drafts/$caseId/household-profile/$householdId/decision-making",
 )({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return (
-    <div>
-      Hello
-      "/_app/data-collection/forms/drafts/$surveyId/household-profile/decision-making"!
-    </div>
-  )
+  return <Decision />;
 }
