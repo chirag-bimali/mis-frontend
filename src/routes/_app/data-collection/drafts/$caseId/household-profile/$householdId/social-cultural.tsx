11453d9a9f437fe2ec2e755b69b5e01a2b5ab24f
chirag-bimali <70309029+chirag-bimali@users.noreply.github.com>
2026-08-16 12:50:04 +0545

    Refactor social-cultural route to use shared component
    - Replace inline route component with shared SocialCultural page component
    - Clean up unused code and formatting

--- src/routes/_app/data-collection/drafts/$caseId/household-profile/$householdId/social-cultural.tsx
+++ src/routes/_app/data-collection/drafts/$caseId/household-profile/$householdId/social-cultural.tsx
@@ -1,16 +1,12 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import SocialCultural from "@pages/data-collection/drafts/household-profile/social-cultural";
 
 export const Route = createFileRoute(
-  '/_app/data-collection/drafts/$caseId/household-profile/$householdId/social-cultural',
+  "/_app/data-collection/drafts/$caseId/household-profile/$householdId/social-cultural",
 )({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return (
-    <div>
-      Hello
-      "/_app/data-collection/forms/drafts/$surveyId/household-profile/social"!
-    </div>
-  )
+  return <SocialCultural />;
 }
